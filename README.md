324a241780d3de7347dc1235981f466976ffaf41
anavarrogamaga <andresnavag93@gmail.com>
2023-07-04 16:00:37 +0200

    :tada: Intial commit and begin project

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center">
+  Andres Navarro Portfolio
+</h1>
+
+<p align="center">
+  <a href="https://andresnavag93.vercel.app/" target="_blank">
+    https://andresnavag93.vercel.app/
+  </a>
+</p>
+
+### Technologies / Tools
+
+- Reactt.Js
+
+### System requirements
+
+- node ^v18.16.1
+- npm ^9.5.1
+
+#### Developed by: Andres Navarro
---
+++ data/data.ts
@@ -0,0 +1,44 @@
+// import { dataFullStack } from './dataFullStack'
+// import { dataVideoGames } from './dataVideoGames'
+
+// export const dataAndres: Record<TResumeId, TResume> = {
+//   'HOME-andres-navarro': {
+//     id: 'HOME-andres-navarro',
+//     title: `Andres Navarro's Website`,
+//     resume: [
+//       `Software Developer with five (5) years of work experience. 
+//       Three (3) years as a Video Games and Augmented Reality Developer using Unity 3D and Vuforia. 
+//       Background as a Full-Stack Developer. 
+//       XR, Blockchain and NFT enthusiast. 
+//       Nowadays, I am looking for a job as a Unity 3D Developer in the video games industry.`,
+//     ],
+//     skills: {
+//       'game Engines': ['Unity3D'],
+//       'augmented Reality': ['Vuforia'],
+//       code: ['C#', 'C++', 'Javascript', 'HTML', 'CSS', 'Python'],
+//       'front-End': ['React', 'Next.Js'],
+//       '3d Modeling': ['Blender3D'],
+//       multiplayer: ['Photon', 'Mirror'],
+//       'back-End': ['Node', 'Express'],
+//       design: ['Figma', 'Illustrator'],
+//       'video Editing': ['PremierPro'],
+//       dapp: ['Truffle', 'Metamask', 'Hardhat', 'Web3.js', 'Solidity'],
+//       devOps: [
+//         'AWS',
+//         'Route53',
+//         'CloudFront',
+//         'S3',
+//         'Vercel',
+//         'Surge',
+//         'Heroku',
+//       ],
+//       databases: ['Mongo', 'PostgreSql', 'MySql'],
+//       languages: ['Spanish', 'English'],
+//     },
+//   },
+// }
+
+// export const allData: Record<TProjectId, TProject> = {
+//   ...dataFullStack,
+//   ...dataVideoGames,
+// }
